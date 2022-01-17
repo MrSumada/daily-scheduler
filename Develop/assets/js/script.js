4735cfec328e674f025b42c4750e9568f2ef21b0
MrSumada <ricksamericana.com>
2022-01-17 03:33:38 -0500

    add drag drop, and current day

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,29 +1,77 @@
 var hourTime;
-var currentTime;
+var currentTime = moment();
+var currentDay = moment().format("MMM Do YYYY");
 
-generateBlocks();
+$("#currentDay").text(currentDay);
 
-function generateBlocks() {
-    for (var i = 5; i < 19; i++) {
+console.log(currentTime);
 
-        var newBlockEl = $("<div>").addClass("row");
-        var newHourDiv = $("<div>").addClass("hour col-2 align-middle");
-        var newHourEl = $("<p>").addClass("hour-time align-middle");
-        var hourText = i + ":00";
-        $(newHourEl).text(hourText);
-        $(newHourDiv).append(newHourEl);
+// var workDayObj = [
+//     {
+//         time: moment({hour: 5}).format("h:mm A"),
+//         text: ""
+//     }]
 
-        var newDescriptionDiv = $("<div>").addClass("description col-8");
-        var newDescriptionEl = $("<textarea>").text("Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis molestiae, at quo atque corporis vitae est corrupti tempore. Laboriosam id culpa unde, voluptatibus quo neque numquam dignissimos impedit quas ullam!");
-        $(newDescriptionDiv).append(newDescriptionEl);
+// generateBlocks();
 
-        var newButtonDiv = $("<div>").addClass("time-block col-2");
-        var newButtonEl = $("<button>").addClass("saveBtn");
-        $(newButtonEl).html('<i class="far fa-calendar-plus"></i>');
-        $(newButtonDiv).append(newButtonEl);
+// function generateBlocks() {
+//     for (var i = 5; i < 24; i++) {
 
-        $(newBlockEl).append(newHourDiv, newDescriptionDiv, newButtonDiv);
+//         var newBlockEl = $("<div>").addClass("row d-flex vw-90");
+//         var newHourDiv = $("<div>").addClass("hour col-2 align-middle");
+//         var newHourEl = $("<p>").addClass("hour-time align-middle");
+//         var hourText = moment.utc({hour: i}).format("h:mm A");
+//         $(newHourEl).text(hourText);
 
-        $(".container").append(newBlockEl);
-    }
+//         $(newHourDiv).append(newHourEl);
+
+//         var newDescriptionDiv = $("<div>").addClass("description col-9");
+//         var newDescriptionEl = $("<textarea>").text("");
+//         $(newDescriptionDiv).append(newDescriptionEl);
+
+//         var newButtonDiv = $("<div>").addClass("time-block col-1");
+//         var newButtonEl = $("<button>").addClass("saveBtn");
+//         $(newButtonEl).html('<i class="far fa-calendar-plus"></i>');
+//         $(newButtonDiv).append(newButtonEl);
+
+//         $(newBlockEl).removeClass("past present future")
+
+//         if (moment(hourText).isAfter(currentTime)) {
+//             $(newBlockEl).addClass("future");
+//         }
+
+//         $(newBlockEl).append(newHourDiv, newDescriptionDiv, newButtonDiv);
+
+//         $(".container").append(newBlockEl);
+//     }
+// }
+
+function saveTextButton() {
+    $(".row").on("click", "button", function(){
+        console.log("This will save it");
+    });
 }
+
+saveTextButton();
+
+// function testTime() {
+//     $(newBlockEl).removeClass("past present future")
+
+//     if (moment().isAfter(currentTime)) {
+//         $(newBlockEl).addClass("future");
+//     }
+// };
+
+$(".row").draggable({
+    helper: "clone",
+    zIndex: 100,
+    revert: true
+    // axis: y
+    // $(this).find("textarea").text("")
+});
+
+$(".row").droppable({
+    drop: function(event, textarea) {
+        $(this).find("textarea").text("Dropped!");
+    }
+});
